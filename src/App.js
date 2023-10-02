71c857c0183ad70b7a785619d510bb2a9203b0f3
jamesparsaie <parsaiejames@gmail.com>
2023-10-02 17:14:53 -0500

    Preparing Search for API and additional headers

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import SearchComponent from './components/SearchComponent';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to your politics helper!</h1>
+      <h1>Welcome to your Politics Helper!</h1>
       <hr></hr>
-      <h2>What is the intention of this app?</h2>
+      <h2><u>What is the intention of this app?</u></h2>
+      <p style={{ maxWidth: '800px', margin: '0 auto' }}>This app is intended to help individuals quickly obtain civic information related to their
+          local area. This includes who represents them in government as well as information about local
+          polling stations
+      </p>
+      <h2><u>Enter your address below to find information</u></h2>
+      <br></br>
       <SearchComponent></SearchComponent>
     </div>
   );
--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
 
 function SearchComponent(){
-      return (
+    const [query, setQuery] = useState(''); // to hold the user input
+  
+    const handleSearch = () => {
+        // Make API call with the user's query here
+    };
+
+    return (
         <div>
-          This is my component!
+        <input 
+            type="text" 
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Enter your address..."
+        />
+        <button onClick={handleSearch}>Search</button>
         </div>
-      );
-  }
+    );
+    }
 
   export default SearchComponent;
