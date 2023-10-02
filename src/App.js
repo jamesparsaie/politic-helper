7abb60c8e91fed23e2d5055003883b27fe9522ab
jamesparsaie <parsaiejames@gmail.com>
2023-10-02 16:56:19 -0500

    Initial changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchComponent from './components/SearchComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to your politics helper!</h1>
+      <hr></hr>
+      <h2>What is the intention of this app?</h2>
+      <SearchComponent></SearchComponent>
     </div>
   );
 }
---
+++ src/components/SearchComponent.js
@@ -0,0 +1,10 @@
+
+function SearchComponent(){
+      return (
+        <div>
+          This is my component!
+        </div>
+      );
+  }
+
+  export default SearchComponent;
